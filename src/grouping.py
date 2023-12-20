2b3ba4f5b9295c7c86021ef5b9dc9d6863ebda85
malliste <malliste@student.21-school.ru>
2023-12-20 16:44:37 +0300

    added distribution to boxes

--- src/grouping.py
+++ src/grouping.py
@@ -1,37 +1,48 @@
 import json
 
-def grouping(sorted_mas):
+def grouping(sorted_mas : list):
     ret_mas = []
     with open("data_file.json", "r") as read_file:
-        r_file = json.load(read_file)
+        r_file : dict = json.load(read_file)
     with open("group_kolvo.json", "r") as ma_kolvo:
-        max_kolvo = json.load(ma_kolvo)
-        # print(sorted_mas)
-    for barcode in sorted_mas:
-        subgroup = max_kolvo[barcode[0]]
-        # print(barcode)
-        if r_file[barcode[2]]['kol-vo'] > subgroup:
-            # while 
-            tmp_mas = []
-            tmp_mas.append(barcode[2])
-            tmp_mas.append(subgroup)
-            r_file[barcode[2]]["kol-vo"] = r_file[barcode[2]]["kol-vo"] - subgroup
-            ret_mas.append(tmp_mas)
-        if r_file[barcode[2]]['kol-vo'] == subgroup:
-            tmp_mas = []
-            tmp_mas.append(barcode[2])
-            tmp_mas.append(r_file[barcode[2]]["kol-vo"])
-            tmp_mas.append("chotko_vlezlo")
-            ret_mas.append(tmp_mas)
-            del r_file[barcode[2]]
-        if barcode[2] in r_file:
-            if r_file[barcode[2]]['kol-vo'] < subgroup:
-                tmp_mas = []
-                tmp_mas.append(barcode[2])
-                tmp_mas.append(r_file[barcode[2]]["kol-vo"])
-                tmp_mas.append("vlezet_eshcho")
-                ret_mas.append(tmp_mas)
-                del r_file[barcode[2]]
+        max_kolvo : dict = json.load(ma_kolvo)
+    item = 0
+    len_mas = len(sorted_mas)
+    # while sorted_mas:
+    while len_mas > item:
+        group = sorted_mas[0][0]
+        # max_size_in_korob : int = max_kolvo[group]
+        max_size_in_korob : int = 10
+        tmp_korob : list = []
+        while max_size_in_korob > 0 and len_mas > item:
+            if r_file[sorted_mas[item][2]]['kol-vo'] == max_size_in_korob:
+                tmp_korob.append(sorted_mas[item][2])  # in func or class
+                tmp_korob.append(max_size_in_korob)
+                max_size_in_korob = 0
+                ret_mas.append(tmp_korob)
+                # sorted_mas.pop(item)
+                item += 1
+                break
+
+            elif r_file[sorted_mas[item][2]]['kol-vo'] > max_size_in_korob:
+                tmp_korob.append(sorted_mas[item][2])
+                tmp_korob.append(max_size_in_korob)
+                ret_mas.append(tmp_korob)
+                r_file[sorted_mas[item][2]]['kol-vo'] = r_file[sorted_mas[item][2]]['kol-vo'] - max_size_in_korob
+                max_size_in_korob = 0
+                break
+
+            elif r_file[sorted_mas[item][2]]['kol-vo'] < max_size_in_korob:
+                tmp_korob.append(sorted_mas[item][2])
+                tmp_korob.append(r_file[sorted_mas[item][2]]['kol-vo'])
+                max_size_in_korob = max_size_in_korob - r_file[sorted_mas[item][2]]['kol-vo']
+                item += 1
+                # sorted_mas.pop(item)
+    item = 1
     for i in ret_mas:
-        print(i)
+        print("korob",item, "---" ,i)
+        item+=1
+
+        
+
 
