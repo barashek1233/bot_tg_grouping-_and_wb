6dbf843eb4d0250570300ac2eaaf05ec0739a769
malliste <malliste@student.21-school.ru>
2023-12-28 09:59:08 +0300

    dev

--- src/up_google.py
+++ src/up_google.py
@@ -1,6 +1,6 @@
 import gspread
 # Указываем путь к JSON
-gc = gspread.service_account(filename='mypython-408913-267a81584536.json')
+gc = gspread.service_account(filename='../mypython-408913-267a81584536.json')
 #Открываем тестовую таблицу
 sh = gc.open("Копия Отгрузки-2")
 #Выводим значение ячейки A1
